e562e3503f94ee5a56e70df8f0cdc0763d92838b
blueroler <107337300+blueroler@users.noreply.github.com>
2023-12-28 22:16:33 +0700

    Update browser.bat

--- browser.bat
+++ browser.bat
@@ -1,5 +1,4 @@
 @echo off
-start chrome.exe https://blueroler.com
-start chrome.exe https://www.youtube.com/watch?v=f08k3jYXM-Y
-start chrome.exe https://www.tiktok.com/@chan_thanh_99/video/7170325479600311579
+start chrome.exe https://blueroler-my.sharepoint.com
+start chrome.exe https://www.tiktok.com
 exit
