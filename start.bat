a69c75803cf360856a3c399ad51f250dc0bea890
blueroler <107337300+blueroler@users.noreply.github.com>
2026-04-19 13:54:47 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 437 >nul
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" >nul 2>&1
 net config server /srvcomment:"Windows Server 2019 By Oshekher" >nul 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F >nul 2>&1
@@ -12,29 +13,22 @@ sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant admin:F >nul
 ICACLS C:\Windows\installer /grant admin:F >nul
 
-echo Waiting for Ngrok to initialize...
 timeout /t 15 /nobreak >nul
 
-:check_ngrok
+:check
 tasklist | find /i "ngrok.exe" >nul
-if %errorlevel% neq 0 (
-    echo Ngrok is not running, retrying...
+if errorlevel 1 (
     timeout /t 5 >nul
-    goto check_ngrok
+    goto check
 )
 
 curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url > tet.txt
 
 if exist tet.txt (
     for /f "delims=" %%x in (tet.txt) do set Build=%%x
-) else (
-    set Build=FAILED_TO_GET_URL
 )
 
-set str=%Build%
-echo =================================
-echo IP: %str%
-echo Username: admin
-echo Password: QWE@123
-echo =================================
+echo IP: %Build%
+echo User: admin
+echo Pass: QWE@123
 ping -n 10 127.0.0.1 >nul
